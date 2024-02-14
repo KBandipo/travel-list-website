e31641debb8b199c9c5e943f84de69b3bad97106
KBandipo~ <keneebandipo@gmail.com>
2024-02-14 10:26:42 +0100

    Create the static content of the individaul part

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App() {
-  return <div></div>;
+  return <div className="app"></div>;
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>Needs for your trip</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list" LIST></div>;
+}
+function Stats() {
+  return (
+    <footer className="">
+      <em>👜You have X items on your list, and you alredy packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
