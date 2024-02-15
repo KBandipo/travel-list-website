6cada523a6367bcc7b0dff074e9402c0952d5e97
KBandipo~ <keneebandipo@gmail.com>
2024-02-15 14:50:15 +0100

    Add array message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+
+
+
 function App() {
   return (
     <div className="app">
@@ -22,11 +30,13 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list" LIST></div>;
+  return <div className="list">{
+    initialItems.map((item)=> )
+  }</div>;
 }
 function Stats() {
   return (
-    <footer className="">
+    <footer className="stats">
       <em>👜You have X items on your list, and you alredy packed X (X%)</em>
     </footer>
   );
